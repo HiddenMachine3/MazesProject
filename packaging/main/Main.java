b28f9f14db4e1dbcb3a3a47708660f8ba93da92b
atesta02496 <113718036+atesta02496@users.noreply.github.com>
2022-10-15 17:54:24 +0530

    made middle man jar code
    Finally got it working

--- packaging/main/Main.java
+++ packaging/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
@@ -24,17 +25,22 @@ public class Main {
             Class<?> mainClass = loader.loadClass("com.threerings.getdown.launcher.GetdownApp");
             Method mainMethod = mainClass.getMethod("main", String[].class);
 
-            System.out.println(appFolderContainingGetdownFiles.exists() + " " + appFolderContainingGetdownFiles.toString());
+            System.out.println(appFolderContainingGetdownFiles.exists() + " can:"
+                    + appFolderContainingGetdownFiles.getCanonicalPath() + " path:"
+                    + appFolderContainingGetdownFiles.getPath() + " absolutePath:"
+                    + appFolderContainingGetdownFiles.getAbsolutePath() + " ");
 
             try {
-                mainMethod.invoke(null,new String[]{"app"});
-                        //new Object[]{appFolderContainingGetdownFiles.toString()});
+                mainMethod.invoke(this, (Object) new String[]{appFolderContainingGetdownFiles.getPath()});
+                //new Object[]{appFolderContainingGetdownFiles.toString()});
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
             } catch (InvocationTargetException e) {
                 e.printStackTrace();
             }
         } catch (ClassNotFoundException | NoSuchMethodException | MalformedURLException | URISyntaxException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
